46e7ecafbc131183664487e3423c9775599725e8
Shakti Lakhera <shakti.lakhera@tigeranalytics.com>
2025-04-18 02:27:13 +0530

    src folder added

---
+++ src/HousePricePrediction/ingest_data.py
@@ -0,0 +1,105 @@
+import os
+import tarfile
+
+import numpy as np
+import pandas as pd
+from six.moves import urllib
+from sklearn.model_selection import StratifiedShuffleSplit
+
+
+def fetch_housing_data(housing_url, housing_path):
+    """
+    This function creates the target directory (if it doesn't exist),
+    downloads the housing dataset from the specified URL as a .tgz archive,
+    and extracts its contents into the specified local path.
+
+    Parameters:
+    ----------
+    housing_url : str
+        The URL to download the housing dataset from.
+    housing_path : str
+        The local directory where the dataset will be saved and extracted.
+
+    Returns:
+    -------
+    None
+    """
+    os.makedirs(housing_path, exist_ok=True)
+    tgz_path = os.path.join(housing_path, "housing.tgz")
+    urllib.request.urlretrieve(housing_url, tgz_path)
+    housing_tgz = tarfile.open(tgz_path)
+    housing_tgz.extractall(path=housing_path)
+    housing_tgz.close()
+
+
+def load_housing_data(housing_path):
+    """
+    This function read the housing dataset from the the target directory.
+
+    Parameters:
+    ----------
+    housing_path : str
+        The local directory where the dataset will be saved and extracted.
+
+    Returns:
+    -------
+    pandas dataframe
+    """
+    csv_path = os.path.join(housing_path, "housing.csv")
+    return pd.read_csv(csv_path)
+
+
+def str_split(housing, test_sizes=0.2):
+    """
+    This function strtified split the data on Median household income
+
+    Parameters:
+    ---------
+    housing: pandas dataframe
+    test_size: train-test split ratio
+
+    Returns:
+    Strat_train_set: Stratified Train set
+    Strat_test_set: Stratified Test set
+    """
+
+    housing["income_cat"] = pd.cut(
+        housing["median_income"],
+        bins=[0.0, 1.5, 3.0, 4.5, 6.0, np.inf],
+        labels=[1, 2, 3, 4, 5],
+    )
+
+    split = StratifiedShuffleSplit(
+        n_splits=1, test_size=test_sizes, random_state=42
+        )
+    for train_index, test_index in split.split(housing, housing["income_cat"]):
+        strat_train_set = housing.loc[train_index]
+        strat_test_set = housing.loc[test_index]
+
+    return strat_train_set, strat_test_set
+
+
+class DataIngestion:
+    def __init__(self):
+        self.DOWNLOAD_ROOT = (
+            "https://raw.githubusercontent.com/ageron/handson-ml/master/"
+        )
+        self.HOUSING_PATH = os.path.join("datasets", "housing")
+        self.HOUSING_URL = self.DOWNLOAD_ROOT + "datasets/housing/housing.tgz"
+
+    def initiate_data_ingestion(self):
+        fetch_housing_data(self.HOUSING_URL, self.HOUSING_PATH)
+
+        housing = load_housing_data(self.HOUSING_PATH)
+
+        strat_train_set, strat_test_set = str_split(housing, 0.2)
+
+        train_path = os.path.join("datasets/housing", "train.csv")
+        test_path = os.path.join("datasets/housing", "test.csv")
+
+        strat_train_set.to_csv(train_path, index=False, header=True)
+        strat_test_set.to_csv(test_path, index=False, header=True)
+
+if __name__ == "__main__":
+    obj = DataIngestion()
+    obj.initiate_data_ingestion()
---
+++ src/HousePricePrediction/score.py
@@ -0,0 +1,100 @@
+import os
+import pickle
+import numpy as np
+import pandas as pd
+from sklearn.metrics import mean_squared_error
+from sklearn.impute import SimpleImputer
+
+
+def get_features(housing):
+    """
+    This function add new features: rooms_per_household,
+                                    bedrooms_per_room,
+                                    population_per_household,
+    in the housing dataset.
+
+    Parameters:
+    ----------
+    housing : pandas dataframe
+              Train data after stratified train_test split
+    Returns:
+    -------
+    housing : pandas dataframe
+              dataframe with 3 new features
+    """
+    housing["rooms_per_household"] = (
+        housing["total_rooms"] / housing["households"]
+    )
+    housing["bedrooms_per_room"] = (
+        housing["total_bedrooms"] / housing["total_rooms"]
+    )
+    housing["population_per_household"] = (
+        housing["population"] / housing["households"]
+    )
+    return housing
+
+
+def SimpleImputing(housing):
+    """
+    This function imputes the numerical feature columns with the median.
+
+    Parameters:
+    ----------
+    housing : pandas dataframe
+              Train data after feature engineering.
+    Returns:
+    -------
+    housing_num : pandas dataframe
+                dataframe with numerical features only
+    X : pandas dataframe
+       dataframe with imputed values
+    """
+    imputer = SimpleImputer(strategy="median")
+
+    housing_num = housing.drop("ocean_proximity", axis=1)
+
+    imputer.fit(housing_num)
+    X = imputer.transform(housing_num)
+    return housing_num, X
+
+
+class Score:
+    def __init__(self, test_path):
+        self.test_path = test_path
+        self.test_df = pd.read_csv(self.test_path)
+
+    def get_model_score(self):
+        housing = self.test_df.drop(
+            "median_house_value", axis=1
+        )  # drop labels for training set
+        housing_labels = self.test_df["median_house_value"].copy()
+
+        # NUMERICAL COLUMNS
+        housing_num, X = SimpleImputing(housing)
+        housing_tr = pd.DataFrame(X,
+                                  columns=housing_num.columns,
+                                  index=housing.index)
+        housing_tr = get_features(housing_tr)
+
+        # CATEGORICAL COLUMNS
+        housing_cat = housing[["ocean_proximity"]]
+        housing_prepared = housing_tr.join(
+            pd.get_dummies(housing_cat, drop_first=True)
+            )
+
+        return housing_prepared, housing_labels
+
+
+if __name__ == "__main__":
+    test_data_path = os.path.join("datasets/housing", "test.csv")
+    obj = Score(test_data_path)
+    data, labels = obj.get_model_score()
+
+    with open("artifacts/final_model.pkl", "rb") as f:
+        model = pickle.load(f)
+
+    final_predictions = model.predict(data)
+    final_mse = mean_squared_error(labels, final_predictions)
+    final_rmse = np.sqrt(final_mse)
+
+    print(f"Test data RMSE score: {final_rmse: .2f}")
